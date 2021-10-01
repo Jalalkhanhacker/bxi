a0f1799a74fb94193c25543b3db2716789302642
Binyamin Binni <53377458+Binyamin-binni@users.noreply.github.com>
2021-10-01 20:20:37 +0500

    Update bxi.py

--- bxi.py
+++ bxi.py
@@ -1,31 +1,22 @@
-try:
-    import os
-    import sys
-    import subprocess
-    import requests
-except ImportError:
-    os.system("pip install requests")
-import requests
-
-v="3.3"
-arch=subprocess.check_output("uname -om", shell=True).decode()
+import os
 os.system("clear")
-print(" Checking Update . . .")
-vf=(requests.get("https://raw.githubusercontent.com/Binyamin-binni/bxi/main/version").text).rstrip()
+print("""
 
-if os.path.isfile("bxi") and v==vf:
-    os.system("chmod 777 bxi && ./bxi")
-else:
-    os.system("rm -rf bxi bxi.py")
-    os.system("curl -L -o bxi.py https://raw.githubusercontent.com/Binyamin-binni/bxi/main/bxi.py")
-    if "Android" in arch:
-        if "arm" in arch:
-            os.system("curl -L -o bxi https://raw.githubusercontent.com/Binyamin-binni/dynamic/main/bxi/arm/bxi")
-            os.system("chmod 777 bxi && ./bxi")
-        elif "aarch" in arch:
-            os.system("curl -L -o bxi https://raw.githubusercontent.com/Binyamin-binni/dynamic/main/bxi/aarch64/bxi")
-            os.system("chmod 777 bxi && ./bxi")
-        else:
-            print(" Unknown architecture, contact with owner.")
-    else:
-        print(" Unknown machine, contact with owner.")
+ Please use the following New commands
+
+pkg update && pkg upgrade
+pkg install git
+rm -rf bxi
+
+git clone https://github.com/binyamin-binni/bxi
+
+cd bxi
+chmod 777 bxi
+./bxi
+
+
+----------------------------------
+ use the same commands one by one
+----------------------------------
+
+""")
